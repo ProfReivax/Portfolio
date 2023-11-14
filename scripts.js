87e1250942bde0ff414854e7a816e6197a333b2e
Xavier <xavier.dupuis08@gmail.com>
2023-11-14 17:56:51 -0500

    Ajout de l'animation sur les separateur et ajout de la section lien vers les autres sites

--- scripts.js
+++ scripts.js
@@ -13,3 +13,19 @@ function leaveTitre(args) {
     separateur.classList.add('retrecir');
     separateur.style.width = '10%';
 }
+
+function hoverTitreLien(args) {
+    var separateur = document.getElementById('separateur-liens');
+    if (separateur.classList.contains('retrecir'))
+        separateur.classList.remove('retrecir');
+    separateur.classList.add('elargir');
+    separateur.style.width = '12%';
+}
+
+function leaveTitreLien(args) {
+    var separateur = document.getElementById('separateur-liens');
+    if (separateur.classList.contains('elargir'))
+        separateur.classList.remove('elargir');
+    separateur.classList.add('retrecir');
+    separateur.style.width = '10%';
+}
